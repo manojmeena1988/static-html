3288c22e4dbbfb3a3b19da0e4f226d38cdc7b01f
manoj <manoj@sumedhasoftech.com>
2021-06-28 18:51:59 +0530

    fixed parallax animation

--- zenegy/assets/js/peoplepage.js
+++ zenegy/assets/js/peoplepage.js
@@ -24,12 +24,28 @@ $(document).ready(function () {
         // vertical: true,
         asNavFor: '.right-slider-teamblue',
     });
-    $(window).scroll(function () {
-        var pos = $(".automated-section").offset().top;
-        var scroll = $(window).scrollTop() - pos;
-        $(".icon-list").css({
-          transform:
-            "translate3d(-50%, -" + scroll / 100 + "%, 0) scale(" + (100 - scroll / 20) / 100 + ")",
-        });              
+
+    const inViewport = (entries, observer) => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+        });
+    };
+
+    const Obs = new IntersectionObserver(inViewport);
+    const obsOptions = {};
+
+    // Attach observer to every [data-inviewport] element:
+    const ELs_inViewport = document.querySelectorAll('.icon-list');
+    ELs_inViewport.forEach(EL => {
+        Obs.observe(EL, obsOptions);
     });
+
+    // $(window).scroll(function () {
+    //     var pos = $(".automated-section").offset().top;
+    //     var scroll = $(window).scrollTop() - pos;
+    //     $(".icon-list").css({
+    //         transform:
+    //             "translate3d(-50%, -" + scroll / 100 + "%, 0) scale(" + (100 - scroll / 20) / 100 + ")",
+    //     });
+    // });
 });
