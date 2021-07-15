2f3facd305100f9d3c6a545197aebefeb64e7de1
Lokesh Tak <sspl.lokesh2020@gmail.com>
2021-07-15 19:07:44 +0530

    product , application , join the team , login pages html

--- Lactosan/assets/js/homepage.js
+++ Lactosan/assets/js/homepage.js
@@ -1,15 +1,10 @@
-$(document).ready(function() {
-    $('.tab-slide-section').slick({
-        infinite: true,
-        slidesToShow:1,
-        slidesToScroll:1,
-        dots: true,
-        infinite: false,
-        arrows: false,
-        // customPaging : function(slider, i) {
-        //     var thumb = $(slider.$slides[i]).data();
-        //     const number = i++;
-        //     return '<a class="dot">'+number+'</a>';
-        //             },
-      });
-});
+// $(document).ready(function() {
+//     $('.tab-slide-section').slick({
+//         infinite: true,
+//         slidesToShow:1,
+//         slidesToScroll:1,
+//         dots: true,
+//         infinite: false,
+//         arrows: false,
+//       });
+// });
