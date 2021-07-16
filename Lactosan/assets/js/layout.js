929b31e0a03bb1012f7ecdb67f6319895d57aa9a
Lokesh Tak <sspl.lokesh2020@gmail.com>
2021-07-16 19:17:25 +0530

    application , filter design , news and other page

--- Lactosan/assets/js/layout.js
+++ Lactosan/assets/js/layout.js
@@ -1,5 +1,24 @@
-// $(document).ready(function(){
-//     $(".close-btn").click(function(){
-//         $(".drop-box").hide();
-//       });
-// });
+$(document).ready(function(){
+
+    $(".filter-tab").on("click", function() {
+        $("body").toggleClass("panel-active");
+        $(".filter-box").fadeIn("fast");
+    });
+
+    $("body ").on("click", ".offcanvas-overlay , .close-filter", function() {
+        $("body").removeClass("panel-active");
+        $(".filter-box").fadeOut("fast");
+    });
+
+    // $(".filter-tab").click(function(){
+    //     $(".filter-box").show();
+    //   });
+    //   $(".close-filter").click(function(){
+    //     $(".filter-box").hide();
+    //   });
+
+
+
+});
+
+
