d12004eda5306cf88b0a3cfb24f317b9afa474df
Lokesh Tak <sspl.lokesh2020@gmail.com>
2021-08-26 19:02:51 +0530

    New sections latest changes

--- Lactosan/assets/js/news-trends.js
+++ Lactosan/assets/js/news-trends.js
@@ -1,20 +1,24 @@
-// $(document).ready(function() {
-//       if (window.matchMedia("(max-width:767px)").matches) {
-//         $('.news-cat-1').slick({
-//             responsive: [
-//               {
-//                 breakpoint:768,
-//                 settings: {
-//                   slidesToShow:1.1,
-//                   slidesToScroll:1,
-//                   infinite: false,
-//                   dots: false
-//                 }
-//               }
-//               // You can unslick at a given breakpoint now by adding:
-//               // settings: "unslick"
-//               // instead of a settings object
-//             ]
-//           });
-//       }
-// });
+$(document).ready(function() {
+    //   if (window.matchMedia("(max-width:767px)").matches) {
+    //     $('.news-cat-1').slick({
+    //         responsive: [
+    //           {
+    //             breakpoint:768,
+    //             settings: {
+    //               slidesToShow:1.1,
+    //               slidesToScroll:1,
+    //               infinite: false,
+    //               dots: false
+    //             }
+    //           }
+    //           // You can unslick at a given breakpoint now by adding:
+    //           // settings: "unslick"
+    //           // instead of a settings object
+    //         ]
+    //       });
+    //   }
+    $("#treadBtn").click(function(){
+        $("#newsform").hide(300);
+        $(".latest-news h3").text("Lorem ipsome dolor sit amet,  consectetur");
+      });
+});
