f6d49e29dd734e3cc983f54d81bd1519ee9a929e
Lokesh Tak <sspl.lokesh2020@gmail.com>
2021-10-02 11:14:22 +0530

    AOS animations ,  banner video apply  , favicon icon and css changes

--- chili/assets/js/homepage.js
+++ chili/assets/js/homepage.js
@@ -1,35 +1,38 @@
-$(document).ready(function() {
-    $(".basic-price .show-details").click(function(){
-        $("#basic-box").slideToggle();
-        $(".basic-price .show-details i").toggleClass("fa-chevron-up fa-chevron-down");
-        var inoText = $(this).find("p");
-        if(inoText.text() === "Skjul detaljer"){
-            inoText.text("Se detaljer");
-        }
-        else{
-            inoText.text("Skjul detaljer");
-        }
-    });
-    $(".standard-price .show-details").click(function(){
-        $("#standard-box").slideToggle();
-        $(".standard-price .show-details i").toggleClass("fa-chevron-up fa-chevron-down");
-        var inoText = $(this).find("p");
-        if(inoText.text() === "Skjul detaljer"){
-            inoText.text("Se detaljer");
-        }
-        else{
-            inoText.text("Skjul detaljer");
-        }
-    });
-    $(".premium-price .show-details").click(function(){
-        $("#premium-box").slideToggle();
-        $(".premium-price .show-details i").toggleClass("fa-chevron-up fa-chevron-down");
-        var inoText = $(this).find("p");
-        if(inoText.text() === "Skjul detaljer"){
-            inoText.text("Se detaljer");
-        }
-        else{
-            inoText.text("Skjul detaljer");
-        }
-    });
+$(document).ready(function () {
+  $(".basic-price .show-details").click(function () {
+    $("#basic-box").slideToggle();
+    $(".basic-price .show-details i").toggleClass(
+      "fa-chevron-up fa-chevron-down"
+    );
+    var inoText = $(this).find("p");
+    if (inoText.text() === "Skjul detaljer") {
+      inoText.text("Se detaljer");
+    } else {
+      inoText.text("Skjul detaljer");
+    }
+  });
+  $(".standard-price .show-details").click(function () {
+    $("#standard-box").slideToggle();
+    $(".standard-price .show-details i").toggleClass(
+      "fa-chevron-up fa-chevron-down"
+    );
+    var inoText = $(this).find("p");
+    if (inoText.text() === "Skjul detaljer") {
+      inoText.text("Se detaljer");
+    } else {
+      inoText.text("Skjul detaljer");
+    }
+  });
+  $(".premium-price .show-details").click(function () {
+    $("#premium-box").slideToggle();
+    $(".premium-price .show-details i").toggleClass(
+      "fa-chevron-up fa-chevron-down"
+    );
+    var inoText = $(this).find("p");
+    if (inoText.text() === "Skjul detaljer") {
+      inoText.text("Se detaljer");
+    } else {
+      inoText.text("Skjul detaljer");
+    }
+  });
 });
--- chili/assets/js/layout.js
+++ chili/assets/js/layout.js
@@ -1,63 +1,76 @@
-
-$(document).ready(function(){
+$(document).ready(function () {
   $(function () {
     $("[data-bs-toggle='tooltip']").tooltip();
   });
   // form validations start ===========================
-    $('#signupForm').validate({
-      messages: {
-        firstName: "Påkrævet felt",
-        lastName: "Påkrævet felt",
-        cEmail: "Påkrævet felt",
-        phoneNumber: "Påkrævet felt",
-        numberofEmployees: "Påkrævet felt"
-        ,land: "Påkrævet felt"
-      },
-        // errorPlacement: function(error,element) {
-        //   return true;
-        // },
-        submitHandler: function(form) {
-            console.log('Submitted')
-          }
-      });
-// mobile menu toggle js start =================================
-   $(".mobile-menu").on("click", function() {
-     $("body").toggleClass("mobile-box-active");
+  $("#signupForm").validate({
+    messages: {
+      firstName: "Påkrævet felt",
+      lastName: "Påkrævet felt",
+      cEmail: "Påkrævet felt",
+      phoneNumber: "Påkrævet felt",
+      numberofEmployees: "Påkrævet felt",
+      land: "Påkrævet felt",
+    },
+    // errorPlacement: function(error,element) {
+    //   return true;
+    // },
+    submitHandler: function (form) {
+      console.log("Submitted");
+    },
   });
-  
-// mobile menu box close when menu scroll click js =================================  
-  $(".mobile-panel .mobile-menuList li a ").on("click", function() {
+  // mobile menu toggle js start =================================
+  $(".mobile-menu").on("click", function () {
     $("body").toggleClass("mobile-box-active");
- });
-
- 
-// tooltip box js start===============================================
-
-  $(document).click(function(event) { 
-    var $target = $(event.target);
-    if(!$target.closest('.info-i').length && 
-      $('.tooltip-box').is(":visible")) {
-        $(".tooltip-box").fadeOut();
-    }        
   });
 
-  $(".info-i").on("click" , function(){
-    if(!$(this).find(".tooltip-box").is(":visible")){
-      $(".info-i").each(function(index , value){
+  // mobile menu box close when menu scroll click js =================================
+  $(".mobile-panel .mobile-menuList li a ").on("click", function () {
+    $("body").toggleClass("mobile-box-active");
+  });
+
+  // tooltip box js start===============================================
+
+  $(document).click(function (event) {
+    var $target = $(event.target);
+    if (
+      !$target.closest(".info-i").length &&
+      $(".tooltip-box").is(":visible")
+    ) {
+      $(".tooltip-box").fadeOut();
+    }
+  });
+
+  $(".info-i").on("click", function () {
+    if (!$(this).find(".tooltip-box").is(":visible")) {
+      $(".info-i").each(function (index, value) {
         $(value).find(".tooltip-box").fadeOut();
       });
     }
     $(this).find(".tooltip-box").fadeToggle();
-  }); 
+  });
 });
 
-
-//same page menu scroll js ====================================== 
-$(document).on('click', 'a[href^="#"][class="scrollbtm"]', function (event) {
+//same page menu scroll js ======================================
+$(document).on("click", 'a[href^="#"][class="scrollbtm"]', function (event) {
   event.preventDefault();
   window.location.hash = $(this).attr("href");
   var headHeight = $(".main-header").outerHeight();
-  $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top - headHeight
-  },400);
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top - headHeight,
+    },
+    400
+  );
+});
+
+
+//scroll top adda and remove css on header ====================================== 
+
+$(window).scroll(function(){
+  var sticky = $('.main-header'),
+      scroll = $(window).scrollTop(),
+      headerHeight = $('.main-header').outerHeight(); 
+  if (scroll >= headerHeight) sticky.addClass('addNewOne');
+  else sticky.removeClass('addNewOne');
 });
